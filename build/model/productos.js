b7ce61f97b888461ce01574ff2fb8ef9fd55cfe9
unknown <agarcia@iespuntadelverde.es>
2022-02-03 22:09:42 +0100

    Creación de los schemas

---
+++ build/model/productos.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProductoDB = void 0;
+const mongoose_1 = require("mongoose");
+// Definimos el Schema
+const productosSchema = new mongoose_1.Schema({
+    _id: {
+        type: Number // para acceder en la subclase
+    },
+    _NombreProducto: {
+        type: String
+    },
+    _Almacenamiento: {
+        type: Array
+    }
+});
+// La colección de la BD (Plural siempre)
+exports.ProductoDB = (0, mongoose_1.model)('productos', productosSchema);
