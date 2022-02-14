9ff419e19ef17f02bfe47df486910c42c990f644
garciaponsadri <agarcia@iespuntadelverde.es>
2022-02-14 07:40:43 +0100

    Sin relleno

--- build/routes/routes.js
+++ build/routes/routes.js
@@ -11,45 +11,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.routes = void 0;
 const express_1 = require("express");
-const clientes_1 = require("../model/clientes");
-const empleados_1 = require("../model/empleados");
 const database_1 = require("../database/database");
 const productos_1 = require("../model/productos");
-const almacenes_1 = require("../model/almacenes");
 const producto_1 = require("../classes/productos/producto");
 const movil_1 = require("../classes/productos/movil");
 const procesador_1 = require("../classes/productos/procesador");
 const ropa_1 = require("../classes/productos/ropa");
 const usuarios_1 = require("../model/usuarios");
-let dSchemaCliente = {
-    _id: null,
-    _nombreCliente: null,
-    _posicion: null
-};
-let dSchemaEmp = {
-    _id: null,
-    _Tipo: null,
-    _Nombre: null,
-    _Antiguedad: null,
-    _JornadaCompl: null,
-};
-let dSchemaRep = {
-    _id: null,
-    _Tipo: null,
-    _Nombre: null,
-    _Antiguedad: null,
-    _JornadaCompl: null,
-    _Experiencia: null,
-    _Repartos: null
-};
-let dSchemaMozo = {
-    _id: null,
-    _Tipo: null,
-    _Nombre: null,
-    _Antiguedad: null,
-    _JornadaCompl: null,
-    _IdAlmacen: null
-};
 let dSchemaProducto = {
     _id: null,
     _NombreProducto: null,
@@ -86,13 +54,6 @@ let dSchemaProcesador = {
     _Almacenamiento: null,
     _GHz: null
 };
-let dSchemaAlmacen = {
-    _id: null,
-    _Posicion: null,
-    _CapacidadMax: null,
-    _Mozos: null,
-    _Repartidores: null
-};
 let dSchemaUser = {
     _id: null,
     _Nombre: null,
@@ -101,157 +62,6 @@ let dSchemaUser = {
 };
 class DatoRoutes {
     constructor() {
-        /**
-         *
-         * Rutas para Aplicación CRUD de Clientes.
-         *
-         */
-        this.addClientes = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            const { id, nombre, posicion } = req.body;
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                dSchemaCliente = {
-                    _id: id,
-                    _nombreCliente: nombre,
-                    _posicion: posicion
-                };
-                const oSchema = new clientes_1.ClienteDB(dSchemaCliente);
-                yield oSchema.save();
-            })).catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
-        this.searchClientes = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                const valor = req.params.id;
-                console.log(mensaje);
-                const query = yield clientes_1.ClienteDB.findOne({ _id: valor });
-                res.json(query);
-            }))
-                // Testeo
-                .catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
-        this.getClientes = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                console.log(mensaje);
-                const query = yield clientes_1.ClienteDB.find({});
-                res.json(query);
-            }))
-                .catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
-        this.deleteClientes = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            const valor = req.params.id;
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                console.log(mensaje);
-                const query = yield clientes_1.ClienteDB.findOneAndDelete({ _id: valor });
-                res.json(query);
-            }))
-                .catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
-        /**
-         *
-         * Rutas para Aplicación CRUD de Empleados.
-         *
-         */
-        this.addEmp = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            const { id, tipo, nombre, antiguedad, jornada } = req.body;
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                dSchemaEmp = {
-                    _id: id,
-                    _Tipo: tipo,
-                    _Nombre: nombre,
-                    _Antiguedad: antiguedad,
-                    _JornadaCompl: jornada
-                };
-                const oSchema = new empleados_1.EmpleadoDB(dSchemaEmp);
-                yield oSchema.save();
-            })).catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
-        this.addRep = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            const { id, tipo, nombre, antiguedad, jornada, experiencia, repartos } = req.body;
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                dSchemaRep = {
-                    _id: id,
-                    _Tipo: tipo,
-                    _Nombre: nombre,
-                    _Antiguedad: antiguedad,
-                    _JornadaCompl: jornada,
-                    _Experiencia: experiencia,
-                    _Repartos: repartos
-                };
-                const oSchema = new empleados_1.EmpleadoDB(dSchemaRep);
-                yield oSchema.save();
-            })).catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
-        this.addMozo = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            const { id, tipo, nombre, antiguedad, jornada, almacen } = req.body;
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                dSchemaMozo = {
-                    _id: id,
-                    _Tipo: tipo,
-                    _Nombre: nombre,
-                    _Antiguedad: antiguedad,
-                    _JornadaCompl: jornada,
-                    _IdAlmacen: almacen
-                };
-                const oSchema = new empleados_1.EmpleadoDB(dSchemaMozo);
-                yield oSchema.save();
-            })).catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
-        this.searchEmp = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                const valor = req.params.id;
-                console.log(mensaje);
-                const query = yield empleados_1.EmpleadoDB.findOne({ _id: valor });
-                res.json(query);
-            }))
-                // Testeo
-                .catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
-        this.getEmp = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                console.log(mensaje);
-                const query = yield empleados_1.EmpleadoDB.find({});
-                res.json(query);
-            }))
-                .catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
-        this.deleteEmp = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            const valor = req.params.id;
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                console.log(mensaje);
-                const query = yield empleados_1.EmpleadoDB.findOneAndDelete({ _id: valor });
-                res.json(query);
-            }))
-                .catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
         /**
          *
          * Rutas para Aplicación CRUD de Productos.
@@ -462,81 +272,12 @@ class DatoRoutes {
                 res.send(mensaje);
             });
         });
-        /**
-         *
-         * Rutas para Aplicación CRUD de Productos.
-         *
-        */
-        this.getAlmacen = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                console.log(mensaje);
-                const query = yield almacenes_1.AlmacenesDB.find({});
-                res.json(query);
-            }))
-                .catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
-        this.searchAlmacen = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            const valor = req.params.id;
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                console.log(mensaje);
-                const query = yield almacenes_1.AlmacenesDB.findOne({ _id: valor });
-                res.json(query);
-            }))
-                .catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
-        this.addAlmacen = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            const { id, posicion, capacidadMax, mozos, repartidores } = req.body;
-            const valor = req.params.id;
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                dSchemaAlmacen = {
-                    _id: id,
-                    _Posicion: posicion,
-                    _CapacidadMax: capacidadMax,
-                    _Mozos: mozos,
-                    _Repartidores: repartidores
-                };
-                const oSchema = new productos_1.ProductoDB(dSchemaProducto);
-                yield oSchema.save();
-            }))
-                .catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
-        this.deleteAlmacen = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            const valor = req.params.id;
-            yield database_1.db.conectarBD()
-                .then((mensaje) => __awaiter(this, void 0, void 0, function* () {
-                console.log(mensaje);
-                const query = yield almacenes_1.AlmacenesDB.findOneAndDelete({ _id: valor });
-                res.json(query);
-            }))
-                .catch((mensaje) => {
-                res.send(mensaje);
-            });
-        });
         this._router = (0, express_1.Router)();
     }
     get router() {
         return this._router;
     }
     misRutas() {
-        this._router.get('/Clientes/get', this.getClientes);
-        this._router.post('/Clientes/add', this.addClientes);
-        this._router.get('/Clientes/search/:id', this.searchClientes);
-        this._router.get('/Clientes/delete/:id', this.deleteClientes);
-        this._router.post('/Empleados/addEmp', this.addEmp);
-        this._router.post('/Empleados/addRep', this.addRep);
-        this._router.post('/Empleados/addMozo', this.addMozo);
-        this._router.get('/Empleados/get', this.getEmp);
-        this._router.get('/Empleados/search/:id', this.searchEmp);
-        this._router.get('/Empleados/delete/:id', this.deleteEmp);
         this._router.get('/Productos/get', this.getProd);
         this._router.get('/Productos/precios', this.getPrecios);
         this._router.post('/Productos/add', this.addProd);
@@ -550,10 +291,6 @@ class DatoRoutes {
         this._router.get('/Users/search/:id', this.addUsers);
         this._router.delete('/Users/delete', this.deleteUsers);
         this._router.get('/Users/list', this.listUsers);
-        this._router.get('/Almacenes/get', this.getAlmacen);
-        this._router.post('/Almacenes/add', this.addAlmacen);
-        this._router.get('/Almacenes/search/:id', this.searchAlmacen);
-        this._router.get('/Almacenes/delete/:id', this.deleteAlmacen);
     }
 }
 const obj = new DatoRoutes();
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,47 +1,12 @@
 import {Request, Response, Router } from 'express'
-import { ClienteDB, iCliente } from '../model/clientes'
-import { EmpleadoDB, iEmpleado, iMozo, iRepartidor } from '../model/empleados'
 import { db } from '../database/database'
 import { ProductoDB, iProducto, iMovil, iProcesador, iRopa, iProducto2 } from '../model/productos'
-import { AlmacenesDB, iAlmacen } from '../model/almacenes'
 import { Producto } from '../classes/productos/producto'
 import { Movil } from '../classes/productos/movil'
 import { procesador } from '../classes/productos/procesador'
 import { Ropa } from '../classes/productos/ropa'
 import { iUsers, UsersDB } from '../model/usuarios'
 
-let dSchemaCliente: iCliente = {
-    _id: null,
-    _nombreCliente: null,
-    _posicion: null
-}
-
-let dSchemaEmp: iEmpleado = {
-    _id: null,
-    _Tipo: null,
-    _Nombre: null,
-    _Antiguedad: null,
-    _JornadaCompl: null,
-}
-
-let dSchemaRep: iRepartidor = {
-    _id: null,
-    _Tipo: null,
-    _Nombre: null,
-    _Antiguedad: null,
-    _JornadaCompl: null,
-    _Experiencia: null,
-    _Repartos: null
-}
-
-let dSchemaMozo: iMozo = {
-    _id: null,
-    _Tipo: null,
-    _Nombre: null,
-    _Antiguedad: null,
-    _JornadaCompl: null,
-    _IdAlmacen: null
-}
 
 let dSchemaProducto : iProducto = {
     _id: null,
@@ -83,14 +48,6 @@ let dSchemaProcesador: iProcesador = {
     _GHz: null
 }
 
-let dSchemaAlmacen: iAlmacen = {
-    _id: null,
-    _Posicion: null,
-    _CapacidadMax: null,
-    _Mozos: null,
-    _Repartidores: null
-}
-
 let dSchemaUser: iUsers = {
     _id: null,
     _Nombre: null,
@@ -106,169 +63,6 @@ class DatoRoutes {
     }
     get router(){
         return this._router
-    }
-
-    /**
-     * 
-     * Rutas para Aplicación CRUD de Clientes.
-     * 
-     */
-
-    private addClientes = async (req: Request, res: Response) => {
-        const {id, nombre, posicion} = req.body
-        await db.conectarBD()
-        .then( async (mensaje) => {
-          dSchemaCliente = {
-              _id: id,
-              _nombreCliente: nombre,
-              _posicion: posicion
-          }
-          const oSchema = new ClienteDB(dSchemaCliente)
-          await oSchema.save()
-        }).catch((mensaje) => {
-            res.send(mensaje)
-        })
-    }
-
-    private searchClientes = async (req: Request, res: Response) => {
-        await db.conectarBD()
-        .then( async (mensaje) => {
-            const valor = req.params.id
-            console.log(mensaje)
-            const query  = await ClienteDB.findOne({_id: valor})
-            res.json(query)
-        })
-        // Testeo
-        .catch((mensaje) => {
-            res.send(mensaje)
-        })
-    }
-
-    private getClientes = async (req: Request, res: Response) => {
-        await db.conectarBD()
-        .then( async (mensaje) => {
-            console.log(mensaje)
-            const query  = await ClienteDB.find({})
-            res.json(query)
-        })
-        .catch((mensaje) => {
-            res.send(mensaje)
-        })
-    }
-
-    private deleteClientes = async (req: Request, res: Response) => {
-        const valor = req.params.id
-        await db.conectarBD()
-        .then( async (mensaje) => {
-            console.log(mensaje)
-            const query  = await ClienteDB.findOneAndDelete({_id: valor})
-            res.json(query)
-        })
-        .catch((mensaje) => {
-            res.send(mensaje)
-        })
-    }
-
-    /**
-     * 
-     * Rutas para Aplicación CRUD de Empleados.
-     * 
-     */
-
-    private addEmp = async (req: Request, res: Response) => {
-        const {id, tipo, nombre, antiguedad, jornada} = req.body
-        await db.conectarBD()
-        .then( async (mensaje) => {
-          dSchemaEmp = {
-              _id: id,
-              _Tipo: tipo,
-              _Nombre: nombre,
-              _Antiguedad: antiguedad,
-              _JornadaCompl: jornada
-          }
-          const oSchema = new EmpleadoDB(dSchemaEmp)
-          await oSchema.save()
-        }).catch((mensaje) => {
-            res.send(mensaje)
-        })
-    }
-
-    private addRep = async (req: Request, res: Response) => {
-        const {id, tipo, nombre, antiguedad, jornada, experiencia, repartos} = req.body
-        await db.conectarBD()
-        .then( async (mensaje) => {
-            dSchemaRep = {
-                _id: id,
-                _Tipo: tipo,
-                _Nombre: nombre,
-                _Antiguedad: antiguedad,
-                _JornadaCompl: jornada,
-                _Experiencia: experiencia,
-                _Repartos: repartos
-          }
-          const oSchema = new EmpleadoDB(dSchemaRep)
-          await oSchema.save()
-        }).catch((mensaje) => {
-            res.send(mensaje)
-        })
-    }
-
-    private addMozo = async (req: Request, res: Response) => {
-        const {id, tipo, nombre, antiguedad, jornada, almacen} = req.body
-        await db.conectarBD()
-        .then( async (mensaje) => {
-            dSchemaMozo = {
-                _id: id,
-                _Tipo: tipo,
-                _Nombre: nombre,
-                _Antiguedad: antiguedad,
-                _JornadaCompl: jornada,
-                _IdAlmacen: almacen
-          }
-          const oSchema = new EmpleadoDB(dSchemaMozo)
-          await oSchema.save()
-        }).catch((mensaje) => {
-            res.send(mensaje)
-        })
-    }
-
-    private searchEmp = async (req: Request, res: Response) => {
-        await db.conectarBD()
-        .then( async (mensaje) => {
-            const valor = req.params.id
-            console.log(mensaje)
-            const query  = await EmpleadoDB.findOne({_id: valor})
-            res.json(query)
-        })
-        // Testeo
-        .catch((mensaje) => {
-            res.send(mensaje)
-        })
-    }
-
-    private getEmp = async (req: Request, res: Response) => {
-        await db.conectarBD()
-        .then( async (mensaje) => {
-            console.log(mensaje)
-            const query  = await EmpleadoDB.find({})
-            res.json(query)
-        })
-        .catch((mensaje) => {
-            res.send(mensaje)
-        })
-    }
-
-    private deleteEmp = async (req: Request, res: Response) => {
-        const valor = req.params.id
-        await db.conectarBD()
-        .then( async (mensaje) => {
-            console.log(mensaje)
-            const query  = await EmpleadoDB.findOneAndDelete({_id: valor})
-            res.json(query)
-        })
-        .catch((mensaje) => {
-            res.send(mensaje)
-        })
     }
 
 
@@ -494,84 +288,8 @@ class DatoRoutes {
     }
 
 
-    /**
-     * 
-     * Rutas para Aplicación CRUD de Productos.
-     * 
-    */
 
-     private getAlmacen = async (req: Request, res: Response) => {
-        await db.conectarBD()
-        .then( async (mensaje) => {
-            console.log(mensaje)
-            const query  = await AlmacenesDB.find({})
-            res.json(query)
-        })
-        .catch((mensaje) => {
-            res.send(mensaje)
-        })
-    }
-
-    private searchAlmacen = async (req: Request, res: Response) => {
-        const valor = req.params.id
-        await db.conectarBD()
-        .then( async (mensaje) => {
-            console.log(mensaje)
-            const query  = await AlmacenesDB.findOne({_id: valor})
-            res.json(query)
-        })
-        .catch((mensaje) => {
-            res.send(mensaje)
-        })
-    }
-
-    private addAlmacen = async (req: Request, res: Response) => {
-        const {id, posicion, capacidadMax, mozos, repartidores} = req.body
-        const valor = req.params.id
-        await db.conectarBD()
-        .then( async (mensaje) => {
-            dSchemaAlmacen = {
-                _id: id,
-                _Posicion: posicion,
-                _CapacidadMax: capacidadMax,
-                _Mozos: mozos,
-                _Repartidores: repartidores
-          }
-          const oSchema = new ProductoDB(dSchemaProducto)
-          await oSchema.save()
-        })
-        .catch((mensaje) => {
-            res.send(mensaje)
-        })
-    }
-
-    private deleteAlmacen = async (req: Request, res: Response) => {
-        const valor = req.params.id
-        await db.conectarBD()
-        .then( async (mensaje) => {
-            console.log(mensaje)
-            const query  = await AlmacenesDB.findOneAndDelete({_id: valor})
-            res.json(query)
-        })
-        .catch((mensaje) => {
-            res.send(mensaje)
-        })
-    }
-
-
-    misRutas(){
-        this._router.get('/Clientes/get', this.getClientes)
-        this._router.post('/Clientes/add', this.addClientes)
-        this._router.get('/Clientes/search/:id', this.searchClientes)
-        this._router.get('/Clientes/delete/:id', this.deleteClientes)
-
-        this._router.post('/Empleados/addEmp', this.addEmp)
-        this._router.post('/Empleados/addRep', this.addRep)
-        this._router.post('/Empleados/addMozo', this.addMozo)
-        this._router.get('/Empleados/get', this.getEmp)
-        this._router.get('/Empleados/search/:id', this.searchEmp)
-        this._router.get('/Empleados/delete/:id', this.deleteEmp)
-    
+    misRutas(){   
         this._router.get('/Productos/get', this.getProd)
         this._router.get('/Productos/precios', this.getPrecios)
         this._router.post('/Productos/add', this.addProd)
@@ -586,11 +304,6 @@ class DatoRoutes {
         this._router.get('/Users/search/:id', this.addUsers)
         this._router.delete('/Users/delete', this.deleteUsers)
         this._router.get('/Users/list', this.listUsers)
-
-        this._router.get('/Almacenes/get', this.getAlmacen)
-        this._router.post('/Almacenes/add', this.addAlmacen)
-        this._router.get('/Almacenes/search/:id', this.searchAlmacen)
-        this._router.get('/Almacenes/delete/:id', this.deleteAlmacen)
     }
 }
 const obj = new DatoRoutes()
